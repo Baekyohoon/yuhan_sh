260a4ec432cdefac9b1ec56d99b91f352dd57d1a
tjdwn518 <tjdwn518@naver.com>
2023-11-28 09:06:06 +0900

    로고 및 메뉴 a태그 작업완료

--- src/main/java/com/example/demo/controller/GoodsController.java
+++ src/main/java/com/example/demo/controller/GoodsController.java
@@ -6,9 +6,39 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GoodsController {
 	
+	@GetMapping("/glist_ball")
+	public String goodslistball() {
+		return "GoodsList_ball";
+	}
+	
+	@GetMapping("/glist_clothes")
+	public String goodslistclothes() {
+		return "GoodsList_clothes";
+	}
+	
 	@GetMapping("/glist")
 	public String goodslist1() {
 		return "GoodsList";
+	}
+	
+	@GetMapping("/glist_gloves")
+	public String goodslistgloves() {
+		return "GoodsList_gloves";
+	}
+	
+	@GetMapping("/glist_shoes")
+	public String goodslistshoes() {
+		return "GoodsList_shoes";
+	}
+	
+	@GetMapping("/glist_socks")
+	public String goodslistsocks() {
+		return "GoodsList_socks";
+	}
+	
+	@GetMapping("/glist_uniform")
+	public String goodslistuniform() {
+		return "GoodsList_uniform";
 	}
 	
 	@GetMapping("/create")
