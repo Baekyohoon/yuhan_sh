1c6fb475afee98bae64bedc2922e15d7af41c5a6
Baekyohoon <qorygnsska@naver.com>
2023-12-06 00:05:22 +0900

    진행

--- src/main/java/com/example/demo/repository/GoodsInfoRepository.java
+++ src/main/java/com/example/demo/repository/GoodsInfoRepository.java
@@ -2,8 +2,9 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.demo.entity.Goods;
 import com.example.demo.entity.GoodsInfo;
 
 public interface GoodsInfoRepository extends JpaRepository<GoodsInfo, Integer>{
-
+	GoodsInfo findByGoods(Goods goods);
 }
