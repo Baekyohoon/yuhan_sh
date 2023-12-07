612a7120705b301dbe3df0ab8917fbc13acaa32e
Baekyohoon <qorygnsska@naver.com>
2023-12-07 14:01:20 +0900

    큐앤에이 리뷰 구현

--- src/main/java/com/example/demo/controller/ReviewController.java
+++ src/main/java/com/example/demo/controller/ReviewController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.entity.Goods;
 import com.example.demo.entity.Review;
@@ -25,26 +26,31 @@ public class ReviewController {
 	@Autowired
 	private GoodsRepository goodsRepository;
 	
-	@GetMapping("/createreview/{gid}")
-	public String CreateReview(@PathVariable int gid, Model model) {
+	@GetMapping("/createreview/{gid}/{oid}")
+	public String CreateReview(@PathVariable int gid, @PathVariable int oid,Model model) {
 		
 		Goods goods = goodsRepository.findByGid(gid);
 		
 		model.addAttribute("star", 0); // 적절한 초기값으로 설정
 	    model.addAttribute("goods", goods);
 	    model.addAttribute("gid",gid);
+	    model.addAttribute("oid",oid);
 		return "createReview";
 		}
 	
-	@PostMapping("/createreview/{gid}")
+	@PostMapping("/createreview/{gid}/{oid}")
 	public String CreateReview2(@PathVariable int gid, 
+								@PathVariable int oid, 
 								@RequestParam("comment") String comment,
 								@RequestParam("star") int star,
+								@RequestParam(name = "file") MultipartFile file,
 								HttpSession session) {
 		String loggedInUserId = (String) session.getAttribute("loggedInUserId");
 		Goods goods = goodsRepository.findByGid(gid);
 		
-		Review createReview = goodsService.createReview(gid, comment, star, loggedInUserId);
+		Review createReview = goodsService.createReview(gid, oid, comment, star, loggedInUserId, file);
+		
+		
 		return "redirect:/goodsreview/{gid}";
 		}
 	}
