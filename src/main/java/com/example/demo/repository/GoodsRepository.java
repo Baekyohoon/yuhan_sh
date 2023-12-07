14c0eae7b2dda2d766eeea89141cbea6405c39f9
tjdwn518 <tjdwn518@naver.com>
2023-12-07 16:38:33 +0900

    병합

--- src/main/java/com/example/demo/repository/GoodsRepository.java
+++ src/main/java/com/example/demo/repository/GoodsRepository.java
@@ -1,9 +1,13 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.demo.entity.Category;
 import com.example.demo.entity.Goods;
 
 public interface GoodsRepository extends JpaRepository<Goods, Integer>{
 	Goods findByGid(int gid);
+	List<Goods> findByCategory_Categoryname(String categoryname);
 }
